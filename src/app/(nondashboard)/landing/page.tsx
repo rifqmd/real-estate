4fb1035c9c0c8451c3bd227e3aeee4bc2f76df83
rifqmd <rifqim110@gmail.com>
2025-11-09 01:20:27 +0700

    feat: landing page hero section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import HeroSection from "./HeroSection";
 
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <HeroSection />\
+    </div>
+  );
 }
--- src/app/(nondashboard)/layout.tsx
+++ src/app/(nondashboard)/layout.tsx
@@ -8,9 +8,12 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="h-full w-full">
       <Navbar />
-      <main className={`h-full flex w-full flex-col pt-[${NAVBAR_HEIGHT}px]`}>
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
         {children}
       </main>
     </div>
